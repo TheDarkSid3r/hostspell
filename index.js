eed4f1786d9836f811830b62a1bb9077d1454614
TheDarkSid3r <33441906+TheDarkSid3r@users.noreply.github.com>
2020-10-16 00:29:42 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+var spell = (word, host) => {
+    var letterPaths = word
+        .split("")
+        .map((l) => {
+            var n = l.toLowerCase();
+            if (host == "quip") n = "".concat(l.toUpperCase(), Math.floor(Math.random() * 3) + 1);
+            return "".concat("audio/", host, "/", n, ".wav");
+        });
+    console.log(letterPaths);
+    var audio = new Crunker();
+    audio
+        .fetchAudio(...letterPaths)
+        .then((buffers) => {
+            return audio.concatAudio(buffers);
+        })
+        .then((merged) => {
+            return audio.export(merged, "audio/mp3");
+        })
+        .then((output) => {
+            Swal.close();
+            new Howl({
+                src: [output.url],
+                format: ["mp3"],
+                autoplay: true
+            });
+            audio.download(output.blob, "".concat(host, "_", word));
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+};
+
+$(() => {
+    var hosts = [
+        { name: "Quiplash 3 (Schmitty)", id: "quip" },
+        { name: "The Devils and the Details", id: "devils" },
+        { name: "Champ'd Up", id: "champd" },
+        { name: "Talking Points", id: "talks" },
+        { name: "Blather 'Round", id: "blather" }
+    ];
+    hosts.forEach((h) => {
+        $(".host-select").append(
+            $("<option/>").attr({ value: h.id }).html(h.name)
+        );
+    });
+    $(".submit").on("click", () => {
+        var val = $(".word-input").val().trim().substring(0, 10).toUpperCase().replace(/[^A-Z]/g, "");
+        if (!val) {
+            Swal.fire({
+                icon: "error",
+                title: "Error",
+                html: "You have to type something! (that includes letters)"
+            });
+            return;
+        }
+        Swal.fire({
+            icon: "info",
+            title: "Generating&hellip;",
+            showConfirmButton: false,
+            allowOutsideClick: false,
+            allowEscapeKey: false,
+            allowEnterKey: false
+        });
+        spell(val, $(".host-select").val());
+    });
+});
